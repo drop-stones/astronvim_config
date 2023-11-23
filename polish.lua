559fdf192aa9df1a162fa98781e8152d021d38f9
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2023-11-23 09:52:40 +0900

    Support .lldbinit

--- polish.lua
+++ polish.lua
@@ -1,3 +1,39 @@
+-- nvim-dap configs
+local dap = require "dap"
+dap.adapters.codelldb = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = vim.fn.stdpath "data" .. "/mason/packages/codelldb/extension/adapter/codelldb",
+    args = { "--port", "${port}" },
+  },
+}
+dap.configurations.c = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+    initCommands = function()
+      local commands = {}
+      local sources = { vim.env.HOME .. "/.lldbinit", vim.fn.getcwd() .. "/.lldbinit" }
+      for idx, source in ipairs(sources) do
+        local file = io.open(source, "r")
+        if file then
+          local command = "command source " .. source
+          table.insert(commands, command)
+          file:close()
+        end
+      end
+      return commands
+    end,
+  },
+}
+dap.configurations.cpp = dap.configurations.c
+dap.configurations.rust = dap.configurations.c
+
 -- This function is run last and is a good place to configuring
 -- augroups/autocommands and custom filetypes also this just pure lua so
 -- anything that doesn't fit in the normal config locations above can go here
