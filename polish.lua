2a6ffda3f7dc90b5cd528cf7592a8c560828b6ae
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2023-12-02 10:31:39 +0900

    Add a script to load polish/*.lua

--- polish.lua
+++ polish.lua
@@ -1,6 +1,7 @@
 -- This function is run last and is a good place to configuring
 -- augroups/autocommands and custom filetypes also this just pure lua so
 -- anything that doesn't fit in the normal config locations above can go here
+local scan = require "plenary.scandir"
 return {
   -- Set up custom filetypes
   -- vim.filetype.add {
@@ -15,10 +16,12 @@ return {
   --   },
   -- }
 
-  require "user.polish.dap",
-  require "user.polish.overseer",
-  require "user.polish.notify",
-  require "user.polish.nvim-dap-projects",
-  require "user.polish.norg",
-  require "user.polish.toggleterm",
+  -- load all files in polish/
+  scan.scan_dir(vim.fn.stdpath "config" .. "/lua/user/polish", {
+    on_insert = function(entry)
+      -- Get filename without extension
+      local filename = entry:match("[^/^\\.]*%.lua"):gsub("%.lua", "")
+      require("user.polish." .. filename)
+    end,
+  }),
 }
