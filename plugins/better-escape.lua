44041d0663244ad9de8a717014842498e16b9578
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2023-12-16 10:55:57 +0900

    Add new mappings (jk and kj in japanese) to better-escape

--- plugins/better-escape.lua
+++ plugins/better-escape.lua
@@ -1,7 +1,7 @@
 return {
   "max397574/better-escape.nvim",
   opts = {
-    mapping = { "jk", "kj" },
+    mapping = { "jk", "kj", "ｊｋ", "ｋｊ" },
     timeout = 300,
     keys = function() return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<Esc>l" or "<Esc>" end,
   },
